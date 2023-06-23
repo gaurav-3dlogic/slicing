49cd4af806fdeb155cc6417767a374aa78ed8478
gaurav-3dlogic <gs3dlogic@gmail.com>
2023-06-23 09:53:18 +0530

    first commit

---
+++ slicing.py
@@ -0,0 +1,8 @@
+string = 'I like to eat apples'
+string[:6] #=> 'I like'
+string[7:13] #=> 'to eat'
+string[0:-1:2] #=> 'Ilk oetape' (every 2nd character)
+
+print(string[:6])
+print(string[7:13])
+print(string[0:-1:2])
